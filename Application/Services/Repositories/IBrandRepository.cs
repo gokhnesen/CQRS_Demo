d64ab63ba9d70c027a6505ae7560f9c55437ed7d
Gokhan <gokhan_esen@hotmail.com.tr>
2024-03-19 23:10:50 +0300

    fix

--- Application/Services/Repositories/IBrandRepository.cs
+++ Application/Services/Repositories/IBrandRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Domain.Entities;
-using Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Persistance/Contexts/BaseDbContext.cs
+++ Persistance/Contexts/BaseDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,18 @@ namespace Persistance.Contexts
 {
     public class BaseDbContext:DbContext
     {
-        protected IConfiguration MyProperty{ get; set; }
+        protected IConfiguration Configuration{ get; set; }
         public DbSet<Brand> Brands { get; set; }
+
+        public BaseDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
