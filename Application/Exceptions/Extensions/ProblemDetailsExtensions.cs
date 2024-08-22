d634e5132826e4acc22786e3b05be8c2c5b968e1
Gökhan esen <gokhan_esen@hotmail.com.tr>
2024-08-22 22:54:40 +0300

    fix

--- Application/Exceptions/Extensions/ProblemDetailsExtensions.cs
+++ Application/Exceptions/Extensions/ProblemDetailsExtensions.cs
@@ -10,7 +10,7 @@ namespace Application.Exceptions.Extensions
 {
     public static class ProblemDetailsExtensions
     {
-        public static string AsJson<TProblemDetail>(this TProblemDetail details)
+         public static string AsJson<TProblemDetail>(this TProblemDetail details)
             where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(details);
     }
 }
