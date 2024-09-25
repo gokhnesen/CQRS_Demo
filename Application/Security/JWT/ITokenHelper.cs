201b9d3b85a56dad3b5b351d6c8627e41cee3ce5
Gökhan esen <gokhan_esen@hotmail.com.tr>
2024-09-25 11:32:26 +0300

    creating access and refresh token

---
+++ Application/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Security.JWT
+{
+    public  interface ITokenHelper
+    {
+        AccessToken CreateToken(User user,IList<OperationClaim> operationClaims);
+        RefreshToken CreateRefreshToken(User user, string ipAddress);
+    }
+}
